d043fa17f6b9d1f7a55a7c15727c57f3b1314aa3
Zhanyao93 <zhanyao1993@gmail.com>
2022-05-27 21:21:13 +0800

    assignemnt completed

---
+++ DrawerApp/App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
+
+function Feed() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed!</Text>
+    </View>
+  );
+}
+
+function Article() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Article!</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function MyDrawer() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Screen name="Article" component={Article} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
